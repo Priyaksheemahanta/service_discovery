8fadd2a18083ff8af4958d60ddfda973955e3c61
priyakshee <priyaksheem96@gmail.com>
2021-09-09 00:07:56 +0530

    basic login 2

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -1,9 +1,10 @@
 package controllers
 
-import com.google.common.hash.Hashing.md5
+import com.google.common.hash.Hashing.sha512
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 
+import java.security.MessageDigest
 import javax.inject.Inject
 
 class LoginController  @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
@@ -13,11 +14,22 @@ class LoginController  @Inject()(cc: ControllerComponents) extends AbstractContr
       val loginId = requestBody.\("email").as[String]
       val password = requestBody.\("password").as[String]
       if(loginId=="test@test.com" && password=="admin"){
-        md5()
-       Ok(Json.obj("msg"->"successful"))
+        val token= hashing(loginId)
+       Ok(Json.obj("msg"->"successful","token"->token))
       }else{
         Unauthorized(Json.obj("msg"->"unsuccessful"))
       }
+  }
+
+  def hashing(input: String):String={
+    def sha1(input1: String): String = {
+      val crypt = MessageDigest.getInstance("SHA-1")
+      crypt.reset()
+      crypt.update(input1.getBytes("UTF-8"))
+      bytes2hex(crypt.digest)
+    }
+    def bytes2hex(bytes: Array[Byte]): String = bytes.map("%02x" format _).mkString
+    sha1(input)
   }
 
 
